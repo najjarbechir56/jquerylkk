fb41283440ae3ac0910559623aaf791ea50950ed
najjarbechir56 <52280871+najjarbechir56@users.noreply.github.com>
2019-07-08 15:59:20 +0100

    Add files via upload

---
+++ SUMMER ACADEMY/js/main.js
@@ -0,0 +1,98 @@
+var Bb= document.getElementById("bechir");
+function B() {
+ Bb.style.fontWeight="bold"   
+}
+function A() {
+    Bb.style.fontStyle="italic"
+}
+function C() {
+    Bb.style.textDecoration="underline"
+}
+var x=document.getElementById("az");
+function ff(){
+    Bb.style.fontSize=x.value
+}
+var h=document.getElementById("zz");
+function fu() {
+    Bb.style.fontFamily=zz.value
+}
+
+
+
+$("#taswira").mouseover(function(){
+    $("#b1").show();
+    $("#taswira").css("opacity","0.7");
+})
+$("#taswira").mouseleave(function(){
+    $("#b1").hide();
+    $("#taswira").css("opacity","1");
+})
+$("#taswira1").mouseover(function(){
+    $("#b2").show();
+    $("#taswira1").css("opacity","0.7");
+})
+    $("#taswira1").mouseleave(function(){
+        $("#b2").hide();
+        $("#taswira1").css("opacity","1");
+    })
+
+    $("#taswira2").mouseover(function(){
+        $("#b3").show();
+        $("#taswira2").css("opacity","0.7");
+    })
+
+        $("#taswira2").mouseleave(function(){
+            $("#b3").hide();
+            $("#taswira2").css("opacity","1");
+    })
+    $("#taswira3").mouseover(function(){
+        $("#b4").show();
+        $("#taswira3").css("opacity","0.7");
+        
+    });
+        $("#taswira3").mouseleave(function(){
+            $("#b4").hide();
+            $("#taswira3").css("opacity","1");
+        } );
+        $("#taswira4").mouseover(function(){
+            $("#b5").show();
+            $("#taswira4").css("opacity","0.7");
+        });
+            $("#taswira4").mouseleave(function(){
+                $("#b5").hide();
+                $("#taswira4").css("opacity","1");
+            } );
+            $("#taswira5").mouseover(function(){
+                $("#b6").show();
+                $("#taswira5").css("opacity","0.7");
+            });
+            $("#taswira5").mouseleave(function(){
+                $("#b6").hide();
+                $("#taswira5").css("opacity","1");
+            } );
+            $("#taswira6").mouseover(function(){
+                $("#b7").show();
+                $("#taswira6").css("opacity","0.7");
+            });
+            $("#taswira6").mouseleave(function(){
+                $("#b7").hide();
+                $("#taswira6").css("opacity","1");
+            } );
+            $("#taswira7").mouseover(function(){
+                $("#b8").show();
+                $("#taswira7").css("opacity","0.7");
+            });
+            $("#taswira7").mouseleave(function(){
+                $("#b8").hide();
+                $("#taswira7").css("opacity","1");
+        
+            } );
+            $("#taswira8").mouseover(function(){
+                $("#b9").show();
+                $("#taswira7").css("opacity","0.7");
+            });
+            $("#taswira8").mouseleave(function(){
+                $("#b9").hide();
+                $("#taswira7").css("opacity","1");
+        
+            } );
